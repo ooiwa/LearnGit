8259f00040bbaa2dd2a30bc66aac3a4d863e5a9e
phamthilinh <phamthilinh@3i.com.vn>
2013-01-24 10:56:03 +0700

    jghfdj

--- DATABASE/src/com/example/database/MainActivity.java
+++ DATABASE/src/com/example/database/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		String a = "hhgkjdhgjk";
+		String a = "hhgkjdhgjkjhshjf";
 	}
 
 	@Override
